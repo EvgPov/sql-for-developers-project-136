33439e19ababae060e9ede8a616c8b8337b5de9c
EvgPov <pev-elets@yandex.ru>
2025-08-20 18:11:42 +0300

    fix columns name

--- database.sql
+++ database.sql
@@ -6,36 +6,36 @@ CREATE EXTENSION IF NOT EXISTS pgcrypto;
 
 CREATE TABLE lessons (
   id bigint PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-  title varchar(255) NOT NULL,
-  body text NOT NULL,
-  link_to_video varchar(255),
-  order_of_lesson integer NOT NULL,
-  create_at timestamptz NOT NULL,
-  update_at timestamptz NOT NULL,
   course_id bigint NOT NULL REFERENCES courses (id),
-  is_deleted boolean NOT NULL DEFAULT false
+  name varchar(255) NOT NULL,
+  content text NOT NULL,
+  video_url varchar(255),
+  position integer NOT NULL,
+  created_at timestamptz NOT NULL,
+  updated_at timestamptz NOT NULL,
+  deleted_at boolean NOT NULL DEFAULT false
 );
 
 CREATE TABLE courses (
   id bigint PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-  title varchar(255) NOT NULL,
-  description_course text NOT NULL,
-  create_at timestamptz NOT NULL,
-  update_at timestamptz NOT NULL,
-  is_deleted boolean NOT NULL DEFAULT false
+  name varchar(255) NOT NULL,
+  description text NOT NULL,
+  created_at timestamptz NOT NULL,
+  updated_at timestamptz NOT NULL,
+  deleted_at boolean NOT NULL DEFAULT false
 );
 
 CREATE TABLE modules (
   id bigint PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
   course_id bigint REFERENCES courses (id) NOT NULL,
-  title varchar(255) NOT NULL,
-  description_module text NOT NULL,
-  create_at timestamptz NOT NULL,
-  update_at timestamptz NOT NULL,
-  is_deleted boolean NOT NULL DEFAULT false
+  name varchar(255) NOT NULL,
+  description text NOT NULL,
+  created_at timestamptz NOT NULL,
+  updated_at timestamptz NOT NULL,
+  deleted_at boolean NOT NULL DEFAULT false
 );
 
-CREATE course_module (
+CREATE course_modules (
   course_id bigint NOT NULL REFERENCES courses (id),
   module_id bigint NOT NULL REFERENCES  modules (id),
   PRIMARY KEY (course_id, module_id)
@@ -44,14 +44,14 @@ CREATE course_module (
 CREATE TABLE programs (
   id bigint PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
   module_id bigint REFERENCES modules (id) NOT NULL,
-  title varchar(255) NOT NULL,
-  cost numeric(10,2) NOT NULL,
-  type_program varchar(255) NOT NULL,
-  create_at timestamptz NOT NULL,
-  update_at timestamptz NOT NULL
+  name varchar(255) NOT NULL,
+  price numeric(10,2) NOT NULL,
+  program_type varchar(255) NOT NULL,
+  created_at timestamptz NOT NULL,
+  updated_at timestamptz NOT NULL
 );
 
-CREATE module_program (
+CREATE program_modules (
   module_id bigint NOT NULL REFERENCES  modules (id),
   program_id bigint NOT NULL REFERENCES programs (id),
   PRIMARY KEY (module_id, programs_id)
@@ -61,20 +61,21 @@ CREATE module_program (
 CREATE TYPE user_role AS ENUM ('student', 'teacher', 'admin');
 CREATE TABLE users (
   id bigint PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-  full_name varchar(255) NOT NULL,
+  name varchar(255) NOT NULL,
   email varchar(255) UNIQUE NOT NULL,
-  password varchar(255) UNIQUE NOT NULL,
+  password_hash varchar(255) UNIQUE NOT NULL,
   teaching_group_id bigint NOT NULL REFERENCES teaching_groups (id),
   role user_role NOT NULL,
-  create_at timestamptz NOT NULL,
-  update_at timestamptz NOT NULL
+  created_at timestamptz NOT NULL,
+  updated_at timestamptz NOT NULL,
+  deleted_at timestamptz NOT NULL
 );
 
 CREATE TABLE teaching_groups (
   id bigint PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-  slag varchar(255) NOT NULL,
-  create_at timestamptz NOT NULL,
-  update_at timestamptz NOT NULL
+  slug varchar(255) NOT NULL,
+  created_at timestamptz NOT NULL,
+  updated_at timestamptz NOT NULL
 );
 
 -- Add tables for user interaction with the platform
@@ -85,8 +86,8 @@ CREATE TABLE enrollments (
   user_id bigint NOT NULL REFERENCES users (id),
   program_id bigint NOT NULL REFERENCES programs (id),
   status enrollments_status NOT NULL,
-  create_at timestamptz NOT NULL,
-  update_at timestamptz NOT NULL
+  created_at timestamptz NOT NULL,
+  updated_at timestamptz NOT NULL
 );
 
 CREATE TYPE payment_status AS ENUM ('pending', 'paid', 'failed', 'refunded');
@@ -95,9 +96,9 @@ CREATE TABLE payments (
   enrollment_id bigint NOT NULL REFERENCES enrollments (id),
   amount numeric(10, 2) NOT NULL,
   status  payment_status NOT NULL,
-  payment_at timestamptz NOT NULL,
-  create_at timestamptz NOT NULL,
-  update_at timestamptz NOT NULL
+  paid_at timestamptz NOT NULL,
+  created_at timestamptz NOT NULL,
+  updated_at timestamptz NOT NULL
 );
 
 CREATE TYPE program_completions_status AS ENUM ('active', 'completed', 'pending', 'cancelled');
@@ -106,10 +107,10 @@ CREATE TABLE program_completions (
   user_id bigint NOT NULL REFERENCES users (id),
   program_id bigint NOT NULL REFERENCES programs (id),
   status program_completions_status NOT NULL,
-  beginning_at timestamptz NOT NULL,
-  ending_at timestamptz NOT NULL
-  create_at timestamptz NOT NULL,
-  update_at timestamptz NOT NULL
+  started_at timestamptz NOT NULL,
+  completed_at timestamptz NOT NULL
+  created_at timestamptz NOT NULL,
+  updated_at timestamptz NOT NULL
 );
 
 CREATE TABLE certificates (
@@ -117,9 +118,9 @@ CREATE TABLE certificates (
   user_id bigint NOT NULL REFERENCES users (id),
   program_id bigint NOT NULL REFERENCES programs (id),
   url varchar(255) NOT NULL,
-  issue_at timestamptz NOT NULL,
-  create_at timestamptz NOT NULL,
-  update_at timestamptz NOT NULL
+  issued_at timestamptz NOT NULL,
+  created_at timestamptz NOT NULL,
+  updated_at timestamptz NOT NULL
 );
 
 -- Create additional content
@@ -127,38 +128,39 @@ CREATE TABLE certificates (
 CREATE TABLE quizzes (
   id bigint PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
   lesson_id bigint NOT NULL REFERENCES lessons (id),
-  title varchar(255) NOT NULL,
-  body varchar(255) NOT NULL,
-  create_at timestamptz NOT NULL,
-  update_at timestamptz NOT NULL
+  name varchar(255) NOT NULL,
+  content varchar(255) NOT NULL,
+  created_at timestamptz NOT NULL,
+  updated_at timestamptz NOT NULL
 );
 
 CREATE TABLE exercises (
   id bigint PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
   lesson_id bigint NOT NULL REFERENCES lessons (id),
-  title varchar(255) NOT NULL,
+  name varchar(255) NOT NULL,
   url varchar(255) NOT NULL,
-  create_at timestamptz NOT NULL,
-  update_at timestamptz NOT NULL
+  created_at timestamptz NOT NULL,
+  updated_at timestamptz NOT NULL
 );
 
 -- Social interaction
 
 CREATE TABLE discussions (
   id bigint PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+  user_id bigint NOT NULL REFERENCES users (id),
   lesson_id bigint NOT NULL REFERENCES lessons (id),
-  body text NOT NULL,
-  create_at timestamptz NOT NULL,
-  update_at timestamptz NOT NULL
+  text text NOT NULL,
+  created_at timestamptz NOT NULL,
+  updated_at timestamptz NOT NULL
 );
 
 CREATE TYPE blog_status AS ENUM ('created', 'in moderation', 'published', 'archived');
-CREATE TABLE blog (
+CREATE TABLE blogs (
   id bigint PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
   user_id bigint NOT NULL REFERENCES users (id),
-  title varchar(255) NOT NULL,
-  body text NOT NULL,
+  name varchar(255) NOT NULL,
+  content text NOT NULL,
   status blog_status NOT NULL,
-  create_at timestamptz NOT NULL,
-  update_at timestamptz NOT NULL
+  created_at timestamptz NOT NULL,
+  updated_at timestamptz NOT NULL
 );
